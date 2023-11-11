63b38b95f2f13021c4cef4e06ea8ea37f3d0a3bd
Egorius188 <postulgin98@mail.ru>
2023-11-11 13:39:10 +0300

    reformat code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public int calculate(double heightInMeters,double weightInKg) {
-        double indexBmi = weightInKg / (heightInMeters*heightInMeters) ;
-        return (int)indexBmi;
+    public int calculate(double heightInMeters, double weightInKg) {
+        double indexBmi = weightInKg / (heightInMeters * heightInMeters);
+        return (int) indexBmi;
     }
 }
